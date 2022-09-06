621aade1719b47d4f554efee4b26d066920e1ad9
Yana <yana.stepani@gmail.com>
2022-09-06 18:08:51 +0700

    task 2 solved

---
+++ Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5, 78 -> третьей цифры нет, 32679 -> 6
+*/
+
+// назначение функции:
+void FindThirdDigit()
+{
+    // получение данных от пользователя
+    Console.WriteLine("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+
+    // преобразование числа в положительное:
+    int numberAbs = Math.Abs(number);
+
+    // проверка условия:
+    if (numberAbs < 100)
+    {
+        Console.WriteLine($"В числе {number} третьей цифры нет.");
+    }
+        else
+        {
+            //int digit3 = numberAbs % 100;
+            while (numberAbs > 1000)
+            {
+            numberAbs = numberAbs / 10;
+            }
+            numberAbs = numberAbs % 10;
+            System.Console.WriteLine($"Третья цифра из числа {number} = {numberAbs}.");
+        }   
+}   
+
+// вывод функции:
+FindThirdDigit();
